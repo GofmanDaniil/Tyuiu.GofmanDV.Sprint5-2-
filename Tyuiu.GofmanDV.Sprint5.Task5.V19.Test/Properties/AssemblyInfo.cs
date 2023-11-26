b21ba671e2f98ccefc404d8631919bada074641b
Tyuiu.GofmanDV@outlook.com <Tyuiu.GofmanDV@outlook.com>
2023-11-26 21:06:50 +0300

    Task5

---
+++ Tyuiu.GofmanDV.Sprint5.Task5.V19.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GofmanDV.Sprint5.Task5.V19.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GofmanDV.Sprint5.Task5.V19.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("973f1297-5663-45c2-ad18-841adc77cb65")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.GofmanDV.Sprint5.Task5.V19/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GofmanDV.Sprint5.Task5.V19.Lib;
+
+namespace Tyuiu.GofmanDV.Sprint5.Task5.V19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Гофман Д. В. | СМАРТб-23-1";
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                            *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                                     *");
+            Console.WriteLine("* Таск #5                                                                              *");
+            Console.WriteLine("* Вариaнт #19                                                                          *");
+            Console.WriteLine("* Выполнил: Гофман Даниил Викторович     | СМАРТб-23-1                                 *");
+            Console.WriteLine("****************************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask5V19.txt";
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("*****************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТAТ:                                                                            *");
+            Console.WriteLine("*****************************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
